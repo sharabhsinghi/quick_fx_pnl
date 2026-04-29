f6c2ac7efe82c9022936ce7ee58d8e526f1b0e30
sharabhsinghi <sharabhsinghi@gmail.com>
2026-04-28 21:33:55 -0400

    Updates

--- _next/static/L0MO68nkwTTEC5fesoBYt/_buildManifest.js
+++ _next/static/L0MO68nkwTTEC5fesoBYt/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/16zmyao2c9eop.js"
+    "static/chunks/16fpiuwprpl9w.js"
   ],
   "/_error": [
     "static/chunks/0b7trlkw36ub7.js"
--- _next/static/chunks/16fpiuwprpl9w.js
+++ _next/static/chunks/16fpiuwprpl9w.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/", [
   "static/chunks/0vr336ynb_e~h.js",
   "static/chunks/16z72w0m_9jw_.js",
   "static/chunks/04b_fzjxgqhaq.js",
-  "static/chunks/17bpgxy9sy82b.js",
-  "static/chunks/turbopack-03i3a-ffsu0fl.js"
+  "static/chunks/0szs7757tzxp6.js",
+  "static/chunks/turbopack-12h289al0l-vo.js"
 ])
